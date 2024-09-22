c9d15880e47369a74daeab7ff06419a71b31759c
Nils Valseth Selte <nilsvselte@gmail.com>
2024-09-22 16:15:50 +0200

    make tema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,76 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [countdown, setCountdown] = useState(100);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(() => {
+    if (countdown > 0) {
+      const timer = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+
+      // Clear the timer when component is unmounted or countdown reaches 0
+      return () => clearInterval(timer);
+    }
+  }, [countdown]);
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center mt-72 justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div>{countdown != 0 && <p className="text-9xl">{countdown}</p>}</div>
+      <div>
+        {countdown > 95 && countdown < 98 && (
+          <>
+            <p>Jaa, dette tok jo litt tid</p>
+          </>
+        )}
+        {countdown > 70 && countdown <= 95 && (
+          <>
+            <p>Du får nesten ta opp mobilen og følge @x_morsom i mellomtiden</p>
+          </>
+        )}
+        {countdown > 50 && countdown <= 70 && (
+          <>
+            <p>Så har du tid til å lese resten av posten på Slack.</p>
+          </>
+        )}
+        {countdown > 40 && countdown <= 50 && (
+          <>
+            <p>Hva med å ta en kaffe?</p>
+          </>
+        )}
+        {countdown > 30 && countdown <= 40 && (
+          <>
+            <p>...eller en øl?</p>
+          </>
+        )}
+        {countdown > 20 && countdown <= 30 && (
+          <>
+            <p>Spennendeeeeee....</p>
+          </>
+        )}
+        {countdown > 10 && countdown <= 20 && (
+          <>
+            <p>...</p>
+          </>
+        )}
+
+        {countdown === 0 && (
+          <>
+            <p className="text-5xl font-bold">SANG</p>
+            <div className="text-lg">
+              <p>Hæææ?? hva skal det bety?</p>
+              <p>Det betyr at du skal ta på kostyme som matcher en sang</p>
+              <p>
+                For eksempel hvis du liker "raining men" kan du ta på en skjorte
+                med menn. Og hvis du liker "I'm blue" kan du ta på blå klær.
+              </p>
+              <p className="font-bold">LYKKE TIL!! Vi sees</p>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
